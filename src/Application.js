9b5d4d38351bf09c4edca45937af71835c4cbe1f
Dvir <44424737+dvirtayeb@users.noreply.github.com>
2021-07-05 12:31:45 +0300

    Update Application.js

--- src/Application.js
+++ src/Application.js
@@ -93,32 +93,3 @@ class Application extends Component {
 }
 
 export default Application;
-
-// class Application extends Component {
-//   constructor() {
-// 		super();
-// 		this.state = {
-// 			open: false
-//     };
-//     Example()
-    
-//   }
-  
-//     render(){  
-//         return (
-//             <BrowserRouter>
-//               <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-//               <SideBar pageWrapId={'page-wrap'} outerContainerId={'outer-container'}/>
-//               <div className="portfolio-header">
-//                 <h1 id="portfolio">My Portfolio!</h1>
-//                 <picture>
-//                   <img id="header-img" src={Header} alt=""/>
-//                 </picture>
-//               </div>
-//               <Menu/>
-//             </BrowserRouter>
-//               );
-//     }
-// }
-
-// export default Application;
