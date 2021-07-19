c96dbd60365683e2c02794b8b65be9410a3d8e58
Dvir <44424737+dvirtayeb@users.noreply.github.com>
2021-07-19 18:19:01 +0300

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
- // function hideProject() {
-        //     var x = document.getElementById({var});
-        //     if (x.style.display === "none") {
-        //       x.style.display = "block";
-        //     } else {
-        //       x.style.display = "none";
-        //     }
-        //   }
